8be515025e8c1745f04d2ba4bd8201becdcc0b5f
jjakpor <14021192+jjakpor@users.noreply.github.com>
2024-01-17 14:10:22 -0800

    changes

--- etch4/Etch/Verification/Semantics/StreamlessBSearch.lean
+++ etch4/Etch/Verification/Semantics/StreamlessBSearch.lean
@@ -36,32 +36,73 @@ theorem searchStep_lt_of_lt {pred pair between} (h : Proper pair) : Proper (sear
 
 theorem adjacent (hmid : mid'_spec_converse mid') (pair : Pair) (lt : Proper pair) : ∃ n, binarySearch pred mid' pair = (n, n + 1) := by
   revert lt
-  have := InvImage.wf size WellFoundedRelation.wf
+  have size_wf := InvImage.wf size WellFoundedRelation.wf
   induction pair using WellFounded.induction
-  . exact this
+  . exact size_wf
   . rename_i pair h
     intro lt
     unfold binarySearch
     cases hm : mid' pair with
-    | none =>
-      have snd_le_fst : pair.2 ≤ pair.1 + 1 := hmid pair hm
-      unfold Proper at lt
-      use pair.fst
-      have lt_or_eq := Nat.eq_or_lt_of_le snd_le_fst
-      cases lt_or_eq
-      . rename_i snd_is_fst_succ
-        have :  pair = (pair.fst, pair.snd) := by simp
-        rw [snd_is_fst_succ] at this
-        assumption
-      . rename_i snd_lt_fst_succ
-        have snd_eq_fst : pair.fst = pair.snd := by
-          have snd_le_fst := Nat.le_of_lt_succ snd_lt_fst_succ
-          have := (@Nat.not_lt pair.fst pair.snd).mpr snd_le_fst
-          contradiction
-        have snd_ne_fst : pair.fst ≠ pair.snd := ne_of_lt lt
-        contradiction
+    | none => exact ⟨pair.1, by simp only [← Nat.le_antisymm (hmid _ hm) lt]⟩
+    | some new =>  apply h _ (searchStep _ _ new.property).property (searchStep_lt_of_lt lt)
+
+abbrev Invariant (pred : ℕ → Bool) (pair : Pair) := ¬pred pair.fst ∧ pred pair.snd
+
+theorem invariant (inv : Invariant pred pair) : Invariant pred (binarySearch pred mid' pair) := by
+  revert inv
+  have size_wf := InvImage.wf size WellFoundedRelation.wf
+  induction pair using WellFounded.induction
+  . exact size_wf
+  . rename_i pair h
+    unfold binarySearch
+    cases hm : mid' pair with
+    | none => simp only [imp_self]
     | some m =>
-      dsimp
+      simp only [hm]
+      intro inv
       apply h
       . exact (searchStep pred pair m.property).property
-      . exact searchStep_lt_of_lt lt
+      . unfold searchStep
+        split <;> dsimp only
+        . rename_i pred_m
+          exact ⟨inv.left, pred_m⟩
+        . rename_i not_pred_m
+          exact ⟨not_pred_m, inv.right⟩
+
+-- searching for target x, using predicate y > x, first elem of returned pair is last value <= x, second elem is virst val > x
+
+def searchPred [LinearOrder α] (arr : List α) (target : α) (i : ℕ) : Bool :=
+  match i with
+  | 0 => false
+  | i + 1 => if h: i < arr.length then arr[i]'h > target else true
+
+lemma not_pred_zero [LinearOrder α] (arr : List α) (target : α) : searchPred arr target 0 = false := rfl
+
+lemma pred_size [LinearOrder α] (arr : List α) (target : α) : searchPred arr target (arr.length + 1) = true := by
+  simp [searchPred]
+
+def searchMid (p : Pair) : Option { m // Between p m } := if h : p.snd - p.fst > 1 then some ⟨p.fst + (p.snd - p.fst) / 2, by
+    unfold Between
+    simp
+    apply And.intro
+    -- I can tell that h is important, which is why I named it, but I don't know how to manipulate these inequalities
+    . sorry
+    . sorry
+  ⟩ else none
+
+def returnIndex [LinearOrder α] (arr : List α) (target : α) := (binarySearch (searchPred arr target) searchMid (0, arr.length + 1)).fst
+
+
+structure InBoundsEq (arr : List α) (target : α) (j : ℕ) : Prop where
+  inBounds : j < arr.length
+  eq : arr[j] = target
+-- Assuming return index is in bounds
+
+/- Todo: prove on paper-/
+theorem bsearch_finds_target_if_target_exists [LinearOrder α] (arr : List α) (target : α) : (∃j, InBoundsEq arr target j) → InBoundsEq arr target (returnIndex arr target) := by
+  intro exists_j
+  constructor
+  . unfold returnIndex
+    done
+  . unfold returnIndex
+    done
