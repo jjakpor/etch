17927c5f259c9070e29d2fb75a346ae08c424feb
jjakpor <14021192+jjakpor@users.noreply.github.com>
2024-02-09 17:59:03 -0800

    Presentation

---
+++ etch4/Etch/Verification/Semantics/PresentationMain.lean
@@ -0,0 +1,77 @@
+import Mathlib.Order.Basic
+import Mathlib.Algebra.Ring.Basic
+import Mathlib.Tactic
+import Aesop
+
+/- A type to represent a pair of indices. -/
+abbrev Pair := ℕ × ℕ
+
+abbrev Proper (pair : Pair) := pair.1 < pair.2
+abbrev Between (pair : Pair) (m : ℕ) := pair.1 < m ∧ m < pair.2
+
+/- The difference between the first and second elements of a pair. Helpful for WF-induction/recursion. -/
+def size : Pair → ℕ := fun p => p.2 - p.1
+
+/- The predicate being used by the binary search. -/
+variable (pred : ℕ → Bool)
+
+/- A single step of binary search. -/
+def searchStep (pair : Pair) (h : pair.fst < m ∧ m < pair.snd) : { p : Pair // size p < size pair } :=
+  if pred m
+    then ⟨(pair.fst, m), let ⟨a, b⟩ := h; tsub_lt_tsub_right_of_le (Nat.le_of_lt a) b⟩
+    else ⟨(m, pair.snd), let ⟨a, b⟩ := h; Nat.sub_lt_sub_left (a.trans b) a⟩
+
+/- A function that optionally returns some m between the elements of a pair.
+   This function should satisfy mid' p = some m ↔ p.2 > p1
+-/
+variable (mid' : (p : Pair) → Option { m // Between p m })
+
+
+def binarySearch (pair : Pair) : Pair :=
+  match mid' pair with
+  | none => pair
+  | some ⟨_, hm⟩ =>
+      let ⟨new, _⟩ := searchStep pred pair hm
+      binarySearch new
+termination_by _ pair => pair.snd - pair.fst
+
+def mid'_spec_converse := ∀p, mid' p = none → p.2 ≤ p.1 + 1
+
+theorem searchStep_lt_of_lt {pred pair between} (h : Proper pair) : Proper (searchStep pred pair between (m := m)) := by
+  unfold searchStep; aesop
+
+theorem adjacent (hmid : mid'_spec_converse mid') (pair : Pair) (lt : Proper pair) : ∃n, binarySearch pred mid' pair = (n, n + 1) := by
+  revert lt
+  have size_wf := InvImage.wf size WellFoundedRelation.wf
+  induction pair using WellFounded.induction
+  . exact size_wf
+  . rename_i pair h
+    intro lt
+    unfold binarySearch
+    cases hm : mid' pair with
+    | none => exact ⟨pair.1, by simp only [← Nat.le_antisymm (hmid _ hm) lt]⟩
+    | some new =>  apply h _ (searchStep _ _ new.property).property (searchStep_lt_of_lt lt)
+
+abbrev Invariant (pred : ℕ → Bool) (pair : Pair) := ¬pred pair.fst ∧ pred pair.snd
+
+theorem invariant (inv : Invariant pred pair) : Invariant pred (binarySearch pred mid' pair) := by
+  revert inv
+  have size_wf := InvImage.wf size WellFoundedRelation.wf
+
+  induction pair using WellFounded.induction
+  . exact size_wf
+  . rename_i pair h
+    unfold binarySearch
+    cases hm : mid' pair with
+    | none => intro; assumption
+    | some m =>
+      simp only [hm]
+      intro inv
+      apply h
+      . exact (searchStep pred pair m.property).property
+      . unfold searchStep
+        split <;> dsimp only
+        . rename_i pred_m
+          exact ⟨inv.left, pred_m⟩
+        . rename_i not_pred_m
+          exact ⟨not_pred_m, inv.right⟩
